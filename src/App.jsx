7be237bdc11b1667c94c8bd868559c204c39c9a6
shrutivd <svd@pdx.edu>
2024-09-01 13:41:51 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,13 @@ function App() {
 			<Navbar />
 			<Routes>
 				<Route path="/" element={<About />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/education" element={<Education />} />
-				<Route path="/experience" element={<Experience />} />
-				<Route path="/project" element={<Project />} />
-				<Route path="/skills" element={<Skills />} />
-				<Route path="/contact" element={<Contact />} />
-				<Route path="/contact" element={<Hamburger />} />
+				<Route path="/PersonalWebsite/about" element={<About />} />
+				<Route path="/PersonalWebsite/education" element={<Education />} />
+				<Route path="/PersonalWebsite/experience" element={<Experience />} />
+				<Route path="/PersonalWebsite/project" element={<Project />} />
+				<Route path="/PersonalWebsite/skills" element={<Skills />} />
+				<Route path="/PersonalWebsite/contact" element={<Contact />} />
+				<Route path="/PersonalWebsite/contact" element={<Hamburger />} />
 			</Routes>
 		</>
 	);
